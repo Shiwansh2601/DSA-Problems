ab64ff87f238218e01e7ec98fa7f49d27dcdaac6
shiwansh123620 <tiwarishiwansh12@gmail.com>
2025-07-19 16:05:15 +0530

    Update palindrome-number.java

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isPalindrome(int x) {
         if(x<0)
         return false;
-        int data=x;
-        int rem;
+        int num=x;
+        int d;
         int rev=0;
-        while(data>0)
+        while(num>0)
         {
-            rem=data%10;
-            rev=rev*10+rem;
-            data=data/10;
+            d=num%10;
+            rev=rev*10+d;
+            num=num/10;
         }
         if(x==rev)
         return true;
